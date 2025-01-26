f33904ac69e5630b9b0f81b77f9e02f47c75a642
ImaneLyachouti <liemyy70@gmail.com>
2025-01-26 17:31:15 +0100

    Update ReducerFavorites.jsx

--- src/Redux/Reducers/ReducerFavorites.jsx
+++ src/Redux/Reducers/ReducerFavorites.jsx
@@ -5,12 +5,12 @@ const initialState = [];
 const favoritesReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TO_FAVORITES":
-      // On vérifie si le parfum existe déjà dans les favoris avant de l'ajouter
+      
       return state.some((item) => item.name === action.payload.name)
         ? state
-        : [...state, action.payload]; // Si déjà dans les favoris, ne rien faire, sinon on l'ajoute
+        : [...state, action.payload];
     case "REMOVE_FROM_FAVORITES":
-      return state.filter((item) => item.name !== action.payload); // Supprime le parfum en utilisant `name`
+      return state.filter((item) => item.name !== action.payload); 
     default:
       return state;
   }
